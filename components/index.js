1fd12a5389eb73861d1fbfd3a9d882acc2d25f07
Stepan <kolodyazhny.s@outlook.com>
2022-08-07 20:21:13 +0400

    1

--- components/index.js
+++ components/index.js
@@ -1,9 +1,21 @@
-AFRAME.registerComponent('testing', {
-  dependencies: ['raycaster'],
-
-  init: function () {
-    this.el.addEventListener('raycaster-intersection', function () {
-      this.setAttribute('material', 'color', 'green');
+AFRAME.registerComponent('raycaster-listen', {
+	init: function () {
+    // Use events to figure out what raycaster is listening so we don't have to
+    // hardcode the raycaster.
+    this.el.addEventListener('raycaster-intersected', evt => {
+      this.raycaster = evt.detail.el;
     });
+    this.el.addEventListener('raycaster-intersected-cleared', evt => {
+      this.raycaster = null;
+    });
+  },
+
+  tick: function () {
+    if (!this.raycaster) { return; }  // Not intersecting.
+
+    let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+    if (!intersection) { return; }
+    console.log(intersection.point);
+    this.setAttribute('material', 'color', 'green');
   }
 });
